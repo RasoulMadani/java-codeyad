902d6ff29125bc3b33826fc7187d68f48fcd8b95
Rasoulmadani <ali.rahbary@chmail.ir>
2024-01-04 00:09:36 +0330

    انواع روش های تعریف متغیر

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int myAge = 24, hisAge = 30 , ali = 22;
+        int herAge = myAge;
+//        int hisAge = 30;
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello allah");
+        System.out.println(herAge);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
