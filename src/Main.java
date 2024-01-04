b58e8dc3644ccebe62fecea98dcb385229a3a176
Rasoulmadani <ali.rahbary@chmail.ir>
2024-01-04 10:50:17 +0330

    کار روی کلاس استرینگ درجاوا

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,8 @@ import java.util.Date;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        int x = 1;
-//        int y = x;
-//        x = 2;
-//        System.out.println("X: " + x);
-//        System.out.println("Y: " + y);
-        Point point1 = new Point(1,1);
-        Point point2 = point1;
-        System.out.println(point1);
-        System.out.println(point2);
+        String hello = new String("hello world");
+        String hello2 = "Hello world !";
+        System.out.println(hello2);
     }
 }
