86cd71e59630fe715f2ce5cef8f342f1305df715
Rasoulmadani <ali.rahbary@chmail.ir>
2024-01-04 08:21:56 +0330

    نحوه ی تعریف متغیرهای نان پریمیتیو یا غیر اولیه و مرجعی

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
+import java.util.Date;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int myAge = 24, hisAge = 30 , ali = 22;
-        int herAge = myAge;
-//        int hisAge = 30;
-
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println(herAge);
-
+        Date now = new Date();
+        System.out.println(now);
     }
 }
