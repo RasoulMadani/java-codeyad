5ef17c65147f2aa157b22767bae8390cb419272b
Rasoulmadani <ali.rahbary@chmail.ir>
2024-01-04 08:42:27 +0330

    مقایسه ی پریمیتیو تایپ ها و رفرنس تایپ ها

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.awt.*;
 import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println(now);
+//        int x = 1;
+//        int y = x;
+//        x = 2;
+//        System.out.println("X: " + x);
+//        System.out.println("Y: " + y);
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        System.out.println(point1);
+        System.out.println(point2);
     }
 }
